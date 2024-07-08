794daaef7dd285c2b645a701ff9113f9b3d407eb
Xiaokui Shu <subbyte@gmail.com>
2024-07-07 21:26:49 -0400

    minor fix

--- packages/kestrel_core/src/kestrel/frontend/compile.py
+++ packages/kestrel_core/src/kestrel/frontend/compile.py
@@ -265,11 +265,12 @@ def _get_entity_entity_relation_specifier_projection(
     else:
         t2 = t1[t1["Relation"] == relation]
         if t2.empty:
+            supported_relations = t1["Relation"].tolist()
             raise UnsupportedObjectRelation(
                 input_type,
                 relation,
                 output_type,
-                f"Supported: {t1["Relation"].tolist()}",
+                f"Supported: {supported_relations}",
             )
         else:
             if df.shape[0] > 1:
